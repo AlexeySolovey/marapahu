aa19e2f9781c5f6bb9398a9d883f813f6d79f524
Alexey <Solovey_alexey3@mail.ru>
2016-07-04 09:23:35 +0300

    finished bottom slider

--- js/common.js
+++ js/common.js
@@ -1,5 +1,16 @@
 ;(function($){
 	$(document).ready(function() {
-		
+		//slider on main pages
+		$('.bxSlider').bxSlider({
+		    auto: true,
+		    autoHover: true,
+		    controls: false,
+		    pager: false,
+		    slideWidth: 300,
+		    minSlides: 2,
+		    maxSlides: 3,
+		    moveSlides: 1,
+		    slideMargin: 10
+		  });
 	});
 })(jQuery)
