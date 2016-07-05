e6eb0eab50c547a2a4484f4c97c44ae2f74a0c52
Gorchak_UA <gorchakua@gmail.com>
2016-07-05 12:53:47 +0300

    php-pages

---
+++ header.php
@@ -0,0 +1,45 @@
+﻿<header class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="social">
+                <p>мы в соц сетях</p>
+                <ul>
+                    <a href="#">
+                        <li><i class="fa fa-facebook"></i></li>
+                    </a>
+                    <a href="#">
+                        <li><i class="fa fa-twitter"></i></li>
+                    </a>
+                    <a href="#">
+                        <li><i class="fa fa-vk"></i></li>
+                    </a>
+                </ul>
+                <button>Заказать тур</button>
+            </div>
+        </div>
+        <div class="col-md-5">
+            <a href="#" class="logo">
+                <img src="images/logo.png" alt="Лого">
+            </a>
+        </div>
+        <div class="col-md-3">
+            <div class="phones">
+                <p>Наши контакты</p>
+                <ul>
+                    <li>
+                        <i class="fa fa-phone"></i>
+                        +38 057 752 38 38
+                    </li>
+                    <li>
+                        <i class="fa fa-phone"></i>
+                        +38 057 752 38 38
+                    </li>
+                    <li>
+                        <i class="fa fa-mobile fa-mobile-last" aria-hidden="true"></i>
+                        +38 057 752 38 38
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
