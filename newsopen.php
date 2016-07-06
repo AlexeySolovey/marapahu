cc67a3b8e9d4274414cebabc7e1e7f3541b51e3c
Alexey <Solovey_alexey3@mail.ru>
2016-07-06 20:25:53 +0300

    06.07.16

---
+++ newsopen.php
@@ -0,0 +1,216 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Marapahu</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bxslider.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <header class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="social">
+                    <p>мы в соц сетях</p>
+                    <ul>
+                        <a href="#">
+                            <li><i class="fa fa-facebook"></i></li>
+                        </a>
+                        <a href="#">
+                            <li><i class="fa fa-twitter"></i></li>
+                        </a>
+                        <a href="#">
+                            <li><i class="fa fa-vk"></i></li>
+                        </a>
+                    </ul>
+                    <button>Заказать тур</button>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <a href="#" class="logo">
+                    <img src="images/logo.png" alt="Лого">
+                </a>
+            </div>
+            <div class="col-md-3">
+                <div class="phones">
+                    <p>Наши контакты</p>
+                    <ul>
+                        <li>
+                            <i class="fa fa-phone"></i>
+                            +38 057 752 38 38
+                        </li>
+                        <li>
+                            <i class="fa fa-phone"></i>
+                            +38 057 752 38 38
+                        </li>
+                        <li>
+                            <i class="fa fa-mobile fa-mobile-last" aria-hidden="true"></i>
+                            +38 057 752 38 38
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section class="navigation">
+        <nav class="navbar">
+            <div class="container">
+                <div class="navbar-header">
+                    <div class="col-xs-12">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#responsive-menu">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                </div>
+                <div class="collapse navbar-collapse" id="responsive-menu">
+                    <ul class="navbar-nav">
+                        <li>
+                            <a href="/index.php">Главная</a>
+                            <div class="jaw">
+                                <div></div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Поиск туров</a>
+                            <div class="jaw">
+                                <div></div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Горячие туры</a>
+                            <div class="jaw">
+                                <div></div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Новости</a>
+                            <div class="jaw">
+                                <div></div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="/contacts.php">Контакты</a>
+                            <div class="jaw">
+                                <div></div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Отзывы</a>
+                            <div class="jaw">
+                                <div></div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </section>
+
+
+    <section class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="news-open-general">
+                        <img src="images/news/karpaty.jpg" />
+                        <nav><a href="news.php"><i class="fa fa-folder-open-o" aria-hidden="true"></i> Новости <div class="triangle"></div></a></nav>
+                        <article>
+                            Благодаря живописной природе и большому выбору горнолыжных курортов туры в 
+                            Карпаты популярны не только в Украине, но и далеко за её пределами. Буковель — идеальное место для катания всей семьёй, здесь есть множество развлечений и возможностей для активного отдыха. Недалеко от Буковеля находится Драгобрат, наиболее высокогорный курорт Карпат, подходящий как для новичков, так и для профессионалов горнолыжного спорта. Городок Яремче интересен начинающим лыжникам, а также поклонникам пешего туризма. 
+                            А для спокойного отдыха и оздоровления идеально подойдёт бальнеологический курорт Поляна.
+                            В горы, на полонины, на Верецкий перевал на лошадях гуцульской породы на 4 дня! 
+                            Настоящий отдых для тех, кто любит лошадей, горы, красивейшие панорамные виды,  
+                            полонины с пьянящим запахом трав. Незабываемые впечатления! Невысокого роста (140-150 см в холке), 
+                            но красивые, умные, грациозные, выносливые, со своеобразной походкой для устойчивости в горной местности  
+                            лошади гуцульской породы отличаются миролюбивым и спокойным характером. У Вас будет надежный друг в пути,
+                             а это, согласитесь, немаловажно.
+                                В горы, на полонины, на Верецкий перевал на лошадях гуцульской породы на 4 дня!
+                                Настоящий отдых для тех, кто любит лошадей, горы, красивейшие панорамные виды,
+                                полонины с пьянящим запахом трав. Незабываемые впечатления! Невысокого роста (140-150 см в холке),
+                                но красивые, умные, грациозные, выносливые, со своеобразной походкой для устойчивости в горной местности
+                                лошади гуцульской породы отличаются миролюбивым и спокойным характером. У Вас будет надежный друг в пути,
+                                а это, согласитесь, немаловажно.
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="pages-navigation">
+        <nav class="open-news-nav">
+            <ul>
+                <li><a href="#" class="active"><i class="fa fa-chevron-left" aria-hidden="true"></i> Предыдущая статья</a></li>
+                <li><a href="#">Следующая статья <i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
+            </ul>
+        </nav>
+    </section>
+
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-push-3 footer-column footer-social">
+                    <p>мы в соц сетях</p>
+                    <ul>
+                        <a href="#">
+                            <li><i class="fa fa-facebook"></i></li>
+                        </a>
+                        <a href="#">
+                            <li><i class="fa fa-twitter"></i></li>
+                        </a>
+                        <a href="#">
+                            <li><i class="fa fa-vk"></i></li>
+                        </a>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-md-pull-6 footer-column">
+                    <p><b>Как нас найти:</b></p>
+                    <p>г.Харьков <br />ул. Полтавский шлях 123, оф. 109</p>
+                    <ul>
+                        <li>
+                            <i class="fa fa-phone"></i>
+                            +38 057 752 18 38
+                        </li>
+                        <li>
+                            <i class="fa fa-phone"></i>
+                            +38 057 752 18 38
+                        </li>
+                        <li>
+                            <i class="fa fa-mobile fa-smart-phone" aria-hidden="true"></i>
+                            +38 057 752 18 38
+                        </li>
+                        <li>
+                            <i class="fa fa-envelope fa-email" aria-hidden="true"></i>
+                            <span>booknanagary@ukr.net</span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-3 footer-column">
+                    <b class="footer-first-b">Когда к нам прийти</b>
+                    <br />
+                    <b>График работы</b>
+                    <br />
+                    <span>Понедельник - пятница</span>
+                    <p>c 10:00 - 18:00</p>
+                    <p>
+                        Cуббота, воскресенье
+                        <br />по предварительной записи
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="js/bxslider.min.js"></script>
+    <script type="text/javascript" src="js/common.js"></script>
+</body>
+</html>
