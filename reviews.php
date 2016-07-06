0bc284fa427c755bfac810f71c2ad5310fdc58d5
Alexey <Solovey_alexey3@mail.ru>
2016-07-06 20:06:12 +0300

    yes

---
+++ reviews.php
@@ -0,0 +1,313 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Marapahu</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bxslider.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <header class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="social">
+                    <p>мы в соц сетях</p>
+                    <ul>
+                        <a href="#">
+                            <li><i class="fa fa-facebook"></i></li>
+                        </a>
+                        <a href="#">
+                            <li><i class="fa fa-twitter"></i></li>
+                        </a>
+                        <a href="#">
+                            <li><i class="fa fa-vk"></i></li>
+                        </a>
+                    </ul>
+                    <button>Заказать тур</button>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <a href="#" class="logo">
+                    <img src="images/logo.png" alt="Лого">
+                </a>
+            </div>
+            <div class="col-md-3">
+                <div class="phones">
+                    <p>Наши контакты</p>
+                    <ul>
+                        <li>
+                            <i class="fa fa-phone"></i>
+                            +38 057 752 38 38
+                        </li>
+                        <li>
+                            <i class="fa fa-phone"></i>
+                            +38 057 752 38 38
+                        </li>
+                        <li>
+                            <i class="fa fa-mobile fa-mobile-last" aria-hidden="true"></i>
+                            +38 057 752 38 38
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section class="navigation">
+        <nav class="navbar">
+            <div class="container">
+                <div class="navbar-header">
+                    <div class="col-xs-12">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#responsive-menu">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                </div>
+                <div class="collapse navbar-collapse" id="responsive-menu">
+                    <ul class="navbar-nav">
+                        <li>
+                            <a href="/index.php">Главная</a>
+                            <div class="jaw">
+                                <div></div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Поиск туров</a>
+                            <div class="jaw">
+                                <div></div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Горячие туры</a>
+                            <div class="jaw">
+                                <div></div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Новости</a>
+                            <div class="jaw">
+                                <div></div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="/contacts.php">Контакты</a>
+                            <div class="jaw">
+                                <div></div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Отзывы</a>
+                            <div class="jaw">
+                                <div></div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </section>
+
+    <header class="rewiews-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <span>Отзывы наших клиентов</span>
+                    <br />
+                    <h3>Главный показатель качества работы</h3>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section class="reviews">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   <article class="review-block">
+                       <div class="review-content">
+                           <div class="review-text">
+                               Таким образом рамки и место обучения кадров в значительной степени
+                               обуславливает создание дальнейших направлений развития. Повседневная практика показывает,
+                               что укрепление и развитие структуры позволяет оценить значение направлений прогрессивного
+                               развития. С другой стороны дальнейшее развитие различных форм деятельности в значительной
+                               степени обуславливает создание форм развития.
+                           </div>
+                           <div class="review-author">
+                               Олександра
+                               <br />
+                               <span>дизайнер</span>
+                           </div>
+                       </div>
+                       <img src="http://gordonua.com/img/article/1138/10_tn.jpg" class="review-img img-circle" />
+                      
+                   </article>
+                    <article class="review-block">
+                        <div class="review-content">
+                            <div class="review-text">
+                                Таким образом рамки и место обучения кадров в значительной степени
+                                обуславливает создание дальнейших направлений развития. Повседневная практика показывает,
+                                что укрепление и развитие структуры позволяет оценить значение направлений прогрессивного
+                                развития. С другой стороны дальнейшее развитие различных форм деятельности в значительной
+                                степени обуславливает создание форм развития.
+                            </div>
+                            <div class="review-author">
+                                Олександра
+                                <br />
+                                <span>дизайнер</span>
+                            </div>
+                        </div>
+                        <img src="http://gordonua.com/img/article/1138/10_tn.jpg" class="review-img img-circle" />
+
+                    </article>
+                    <article class="review-block">
+                        <div class="review-content">
+                            <div class="review-text">
+                                Таким образом рамки и место обучения кадров в значительной степени
+                                обуславливает создание дальнейших направлений развития. Повседневная практика показывает,
+                                что укрепление и развитие структуры позволяет оценить значение направлений прогрессивного
+                                развития. С другой стороны дальнейшее развитие различных форм деятельности в значительной
+                                степени обуславливает создание форм развития.
+                            </div>
+                            <div class="review-author">
+                                Олександра
+                                <br />
+                                <span>дизайнер</span>
+                            </div>
+                        </div>
+                        <img src="http://gordonua.com/img/article/1138/10_tn.jpg" class="review-img img-circle" />
+
+                    </article>
+                    <article class="review-block">
+                        <div class="review-content">
+                            <div class="review-text">
+                                Таким образом рамки и место обучения кадров в значительной степени
+                                обуславливает создание дальнейших направлений развития. Повседневная практика показывает,
+                                что укрепление и развитие структуры позволяет оценить значение направлений прогрессивного
+                                развития. С другой стороны дальнейшее развитие различных форм деятельности в значительной
+                                степени обуславливает создание форм развития.
+                            </div>
+                            <div class="review-author">
+                                Олександра
+                                <br />
+                                <span>дизайнер</span>
+                            </div>
+                        </div>
+                        <img src="http://gordonua.com/img/article/1138/10_tn.jpg" class="review-img img-circle" />
+                    </article>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <section class="pages-navigation">
+        <nav class="news-nav">
+            <ul>
+                <li><a href="#" class="active">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#" class="last"><i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
+            </ul>
+        </nav>
+    </section>
+
+    <section class="review-send-general">
+        <div class="container">
+            <hr />
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="review-send-block">
+                        <header>
+                            <span>Оставьте свой отзыв</span>
+                            <br />
+                            <span>Поделитесь своим впечатлением от работы с нами</span>
+                        </header>
+                        <article class="container">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <input type="text" placeholder="Ваше имя" />
+                                    <br />
+                                    <input type="text" placeholder="Ваша проффесия" />
+                                    <br />
+                                    <textarea placeholder="Ваше мнение о нашей компании"></textarea>
+                                </div>
+                                <div class="col-md-4">
+                                    <img src="images/review-girl.png" />
+                                    <br />
+                                    <span>Добавьте ваше фото</span>
+                                    <button class="btn">Прикрепить файл</button>
+                                </div>
+                            </div>
+                           
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-push-3 footer-column footer-social">
+                    <p>мы в соц сетях</p>
+                    <ul>
+                        <a href="#">
+                            <li><i class="fa fa-facebook"></i></li>
+                        </a>
+                        <a href="#">
+                            <li><i class="fa fa-twitter"></i></li>
+                        </a>
+                        <a href="#">
+                            <li><i class="fa fa-vk"></i></li>
+                        </a>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-md-pull-6 footer-column">
+                    <p><b>Как нас найти:</b></p>
+                    <p>г.Харьков <br />ул. Полтавский шлях 123, оф. 109</p>
+                    <ul>
+                        <li>
+                            <i class="fa fa-phone"></i>
+                            +38 057 752 18 38
+                        </li>
+                        <li>
+                            <i class="fa fa-phone"></i>
+                            +38 057 752 18 38
+                        </li>
+                        <li>
+                            <i class="fa fa-mobile fa-smart-phone" aria-hidden="true"></i>
+                            +38 057 752 18 38
+                        </li>
+                        <li>
+                            <i class="fa fa-envelope fa-email" aria-hidden="true"></i>
+                            <span>booknanagary@ukr.net</span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-3 footer-column">
+                    <b class="footer-first-b">Когда к нам прийти</b>
+                    <br />
+                    <b>График работы</b>
+                    <br />
+                    <span>Понедельник - пятница</span>
+                    <p>c 10:00 - 18:00</p>
+                    <p>
+                        Cуббота, воскресенье
+                        <br />по предварительной записи
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="js/bxslider.min.js"></script>
+    <script type="text/javascript" src="js/common.js"></script>
+</body>
+</html>
